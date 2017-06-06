820f19c64033cbb1886e8335204c2eacf98ea759
dataDiet <akiramadono@gmail.com>
2017-06-06 14:39:03 -0700

    Algorithm Written First Draft

--- src/arrayPartition1.py
+++ src/arrayPartition1.py
@@ -1,11 +1,10 @@
 class Solution(object):
-    def ArrayPairSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-
-        # your solution here
-
-        return 0
-
+	def ArrayPairSum(self, nums):
+		nums=sorted(nums)
+		ls=[]
+		while nums:
+			ls.append(min(nums.pop(),nums.pop()))
+        	# your solution here
+		return(sum(ls))
+s=Solution()
+s.ArrayPairSum([1,4,3,2])
